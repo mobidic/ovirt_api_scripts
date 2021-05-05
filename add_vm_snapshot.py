e28fc10c4c1903590b0f40ead5a06a6a02e1f09b
beboche <david.baux@inserm.fr>
2021-05-05 18:07:19 +0200

    doc

--- add_vm_snapshot.py
+++ add_vm_snapshot.py
@@ -1,28 +1,15 @@
-#!/usr/bin/python
-
-#
-# Copyright (c) 2016 Red Hat, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 import logging
 import argparse
 import datetime
 import ovirtsdk4 as sdk
 import ovirtsdk4.types as types
-
+# custom config
 import config
+
+
+# This script will trigger snapshots or OVA export on an ovirt architecture
+# and a specific VM or all Vms present except the HostedEngine
+
 
 logging.basicConfig(level=logging.DEBUG, filename='example.log')
 
@@ -70,8 +57,8 @@ def export_ova(connection, vms_service, vm, arch_type, current_date):
 
 def main():
     parser = argparse.ArgumentParser(
-        description='Checks for ANNOVAR resources distant updates and convert to ANNOVAR format',
-        usage='python update_resources.py <-d clinvar> <-hp /path/to/annovar/humandb> <-g [GRCh37|GRCh38]> <-a path/to/annovar>'
+        description='Connects to ovirt and triggers ova export or snaphots',
+        usage='python update_resources.py <-a [odev|ovirt]> <-n vmname> <-t [snapshot|ova]>'
     )
     parser.add_argument('-a', '--arch-type', default='odev', required=True,
         help='architecture to realise the snapshot on [odev|ovirt], default=odev')
